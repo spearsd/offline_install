6565e733e193f17e0ebd7666b8f8880c4fbea073
Dustin <dustin.spears@live.com>
2017-08-30 10:35:25 -0400

    Updated directory usage.

--- offline-install.sh
+++ offline-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 STILL_DEPENDENCIES=true
 PACKAGE=$1
+WORKING_DIR=$(pwd)/$PACKAGE
 if [ -z $PACKAGE ]; then
     echo "No package passed." 2>&1
    exit 1
@@ -9,37 +10,41 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root user." 2>&1
    exit 1
 fi
+mkdir $WORKING_DIR
+if [[ ! -d $WORKING_DIR ]]; then
+    echo "Cannot create $WORKING_DIR. Quiting!"
+    exit 1
+fi
 dl_dependencies() {
     STILL_DEPENDENCIES=false
-    echo "Downloading dependencies for rpms located in /tmp/$PACKAGE..."
-    ls /tmp/$PACKAGE | while read line; do
+    echo "Checking for additional dependency rpms required..."
+    ls $WORKING_DIR | while read line; do
         echo "Testing $line..."
-        rpm -Uvh --test /tmp/$PACKAGE/$line &> /tmp/offline_install_output
-        if [[ ! -z "$(cat /tmp/offline_install_output | grep 'is needed by' | cut -d ' ' -f 1 | cut -d '(' -f 1 | sort -u | sed -e 's/^[[:space:]]*//')" ]]; then
+        rpm -Uvh --test $WORKING_DIR/$line &> offline_install_output
+        if [[ ! -z "$(cat offline_install_output | grep 'is needed by' | cut -d ' ' -f 1 | cut -d '(' -f 1 | sort -u | sed -e 's/^[[:space:]]*//')" ]]; then
             STILL_DEPENDENCIES=true
-            cat /tmp/offline_install_output | grep 'is needed by' | cut -d ' ' -f 1 | cut -d '(' -f 1 | sort -u | sed -e 's/^[[:space:]]*//' | while read line2; do
+            cat offline_install_output | grep 'is needed by' | cut -d ' ' -f 1 | cut -d '(' -f 1 | sort -u | sed -e 's/^[[:space:]]*//' | while read line2; do
                 echo "Downloading dependency $line2..."
-                yum install --downloadonly --downloaddir=/tmp/$PACKAGE $line2 &> /tmp/offline_install_output.log
+                yum install --downloadonly --downloaddir=$WORKING_DIR $line2 &> offline_install_output.log
             done
         fi
     done
     return 0
 }
-
-yum install -y yum-plugin-downloadonly &> /tmp/offline_install_output.log
-mkdir /tmp/$PACKAGE
-yum install --downloadonly --downloaddir=/tmp/$PACKAGE $PACKAGE &> /tmp/offline_install_output.log
+echo "Installing yum-plugin-downloadonly..."
+yum install -y yum-plugin-downloadonly &> offline_install_output.log
+yum install --downloadonly --downloaddir=$WORKING_DIR $PACKAGE &> offline_install_output.log
 while $STILL_DEPENDENCIES; do 
     dl_dependencies
 done
-rm -f /tmp/offline_install_output
-rm -f /tmp/offline_install_output.log
-rm -f /tmp/yum_save*
-ls /tmp/$PACKAGE | grep 'i686' | while read line; do
+rm -f offline_install_output
+rm -f offline_install_output.log
+rm -f yum_save*
+ls $PACKAGE | grep 'i686' | while read line; do
     echo "Removing $line! (not needed)"
     rm -f /tmp/$PACKAGE/$line
 done
-tar -czvf $PACKAGE.tar.gz /tmp/$PACKAGE &> /tmp/offline_install_output.log
-rm -f /tmp/offline_install_output.log
-rm -r -f /tmp/$PACKAGE
+tar -czvf $PACKAGE.tar.gz $PACKAGE &> offline_install_output.log
+rm -f offline_install_output.log
+rm -r -f $PACKAGE
 echo "$PACKAGE.tar.gz has been created."
